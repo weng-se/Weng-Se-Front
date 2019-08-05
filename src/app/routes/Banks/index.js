aaa1a1e87ae178a46be8a5a13411660a63ae14f6
younes belhit <younesbelhit@gmail.com>
2019-08-05 13:36:17 +0100

    create bank folder structure

---
+++ src/app/routes/Banks/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Redirect, Route, Switch} from 'react-router-dom';
+import asyncComponent from '../../../util/asyncComponent';
+
+const Banks = ({match}) => (
+    <div className="app-wrapper">
+        <Switch>
+            <Route path={`${match.url}/lists`} component={asyncComponent(() => import('./routes/lists'))}/>
+            <Route path={`${match.url}/create`} component={asyncComponent(() => import('./routes/create'))}/>
+            <Route path={`${match.url}/import`} component={asyncComponent(() => import('./routes/import'))}/>
+        </Switch>
+    </div>
+);
+
+export default Banks;
+
+
